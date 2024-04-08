97420deafec198b8e002fec96edb20ea79fe6753
Tom <xnosx@hotmail.fr>
2024-04-08 15:35:05 +0200

    Update SQL migration IDs in buffer tables

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php include './../src/Pages/includes/header.html'; ?>
-
+<main>
+  <div class="container bg-light p-3 rounded mt-5">
+    <div class="text-center"><h2>Bienvenue</h2></div>
+    <form>
+      <div class="mb-3">
+        <label for="mail" class="form-label">Email*</label>
+        <input type="email" class="form-control" id="mail" aria-describedby="emailHelp">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Mot de passe*</label>
+        <input type="password" class="form-control" id="password">
+      </div>
+      <button type="submit" class="btn btn-primary">Connexion</button>
+    </form>
+  </div>
+</main>
 <?php include './../src/Pages/includes/footer.html'; ?>
