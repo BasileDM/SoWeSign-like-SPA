ac40e811936a8f7774e87f936281103ec6d59ee9
Tom <xnosx@hotmail.fr>
2024-04-09 09:40:58 +0200

    update Htaccess rule for back end

--- back-end/src/router.php
+++ back-end/src/router.php
@@ -1,12 +1,23 @@
 <?php
 
-$url = parse_url($_SERVER['REQUEST_URI']);
+$url = $_SERVER['REQUEST_URI'];
+$url = parse_url($url, PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($url) {
-  case '/':
+
+  case HOME_URL:
+    var_dump($url);
     echo 'Hello, World!';
     break;
+    
+  case HOME_URL . '/a':
+    var_dump($url);
+    echo 'nope';
+    break;
+
   default:
+  var_dump($url);
+  die();
     http_response_code(404);
 }
