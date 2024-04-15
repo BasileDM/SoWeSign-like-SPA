ae2460568781f3dcf03f8371236228c54e1230d2
Tom <xnosx@hotmail.fr>
2024-04-15 11:49:17 +0200

    Fix action links

--- front-end/assets/js/components/promTableRow.js
+++ front-end/assets/js/components/promTableRow.js
@@ -5,6 +5,7 @@ export function promTableRow(content, role) {
   const tdend = document.createElement("td");
   const tdplaces = document.createElement("td");
   const tdactions = document.createElement("td");
+  const actionDiv = document.createElement("div");
   const a1 = document.createElement("a");
   const a2 = document.createElement("a");
   const a3 = document.createElement("a");
@@ -14,21 +15,26 @@ export function promTableRow(content, role) {
   tdstart.textContent = content.START_DATE;
   tdend.textContent = content.END_DATE;
   tdplaces.textContent = content.AVAILABLE_SPOTS;
+  actionDiv.className = "d-flex gap-2 justify-content-end flex-wrap";
   a1.textContent = "Voir";
   a2.textContent = "Modifier";
   a3.textContent = "Supprimer";
   a1.setAttribute("href", "./voir");
   a2.setAttribute("href", "./modifier");
   a3.setAttribute("href", "./supprimer");
+  a1.className = "link-info link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover";
+  a2.className = "link-info link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover";
+  a3.className = "link-danger link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover";
 
   component.appendChild(th);
   component.appendChild(tdstart);
   component.appendChild(tdend);
   component.appendChild(tdplaces);
   component.appendChild(tdactions);
-  tdactions.appendChild(a1);
-  tdactions.appendChild(a2);
-  tdactions.appendChild(a3);
+  tdactions.appendChild(actionDiv);
+  actionDiv.appendChild(a1);
+  actionDiv.appendChild(a2);
+  actionDiv.appendChild(a3);
 
   return component;
 }
