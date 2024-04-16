4d923d301803029171b309b65e3426dce45940e5
Tom <xnosx@hotmail.fr>
2024-04-16 15:27:34 +0200

    Add form creation logic

--- front-end/assets/js/components/customForm.js
+++ front-end/assets/js/components/customForm.js
@@ -1,7 +1,35 @@
 export function customForm(content, role, type) {
+  let inputAmmount = null;
+  let inputNames = [];
+  let inputTypes = [];
+
   switch (type[0]) {
-    case "prom":
-      const divAmmount = 5;
+    case "promotion":
+      inputAmmount = 4;
+      inputNames = ["Nom de la promotion", "Date de début", "Date de fin", "Places disponibles"];
+      inputTypes = ["text", "date", "date", "number"];
+      break;
+
+    default:
+      break;
   }
 
+  const component = document.createElement("form");
+  component.className = "d-flex flex-column gap-3";
+  
+  for (let i = 0; i < inputAmmount; i++) {
+    const div = document.createElement("div");
+    div.className = "mb-3";
+    const label = document.createElement("label");
+    label.className = "form-label";
+    label.innerHTML = inputNames[i];
+    const input = document.createElement("input");
+    input.className = "form-control";
+    input.name = inputNames[i];
+    input.type = inputTypes[i];
+    div.appendChild(label);
+    div.appendChild(input);
+    component.appendChild(div);
+  }
+  return component;
 }
