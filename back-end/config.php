8594d758633121d671010ed7a3bee2576348fd2c
Tom <xnosx@hotmail.fr>
2024-04-17 14:59:39 +0200

    Switch config to true

--- back-end/config.php
+++ back-end/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('DB_INITIALIZED', FALSE);
+    define('DB_INITIALIZED', TRUE);
     define('JWT_SECRET', 'simplon-sws-6534');
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'sws');
